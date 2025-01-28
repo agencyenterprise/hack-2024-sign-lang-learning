4231f3b42834e04944fa1730ad5e9b666d69a50a
Vinicius Borges <vinicius.borges@ae.studio>
2025-01-28 17:17:46 -0300

    refactor: v2 with NextJS

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "My Project",
+  description: "Created with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="h-full">
+      <body
+        className={cn(
+          "min-h-full bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
